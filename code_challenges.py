9f6962b48691990a7de21d4bd3173ccd93350d19
Perceus-Maximus <168503804+Perceus-Maximus@users.noreply.github.com>
2024-07-03 22:15:27 -0400

    Update code_challenges.py

--- code_challenges.py
+++ code_challenges.py
@@ -3,3 +3,16 @@
 def some_function(some_input1, some_input2):
   # … do something with the inputs …
   return output
+
+#Example that retirns the sum of the first and last elements of a given list
+def first_plus_last(lst):
+  return lst[0] + lst[-1]
+  
+#### OUTPUT
+#### >>> first_plus_last([1, 2, 3, 4])
+#### 5
+#### >>> first_plus_last([8, 2, 5, -8])
+#### 0
+#### >>> first_plus_last([-10, 2, 3, -4])
+#### -14
+
